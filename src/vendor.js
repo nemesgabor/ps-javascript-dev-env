7261d99df24b97d15df38cc05b7fe657367c95c4
nemesgabor <nemesgabor@outlook.com>
2017-03-29 20:55:05 +0200

    Prod build basics

---
+++ src/vendor.js
@@ -0,0 +1,4 @@
+/* 3rd party toolok jönnek ide, jquery, bootstrap, stb.  */
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
---
+++ webpack.config.prod.js
@@ -0,0 +1,63 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPLugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: {
+  vendor: path.resolve(__dirname,'src/vendor'),
+   main: path.resolve(__dirname, 'src/index')
+  },
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].js'
+  },
+  plugins: [
+    //Kulon css fajl
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    // Hash md5
+    new WebpackMd5Hash(),
+
+    // CommonChunk a bundle splithez
+    new webpack.optimize.CommonsChunkPlugin({
+      name:'vendor'
+    }),
+
+    //Html plugin
+    new HtmlWebpackPLugin({
+      template:'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject:true
+    }),
+
+    //DeDupe
+    new webpack.optimize.DedupePlugin(),
+
+    //Minify
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap') }
+    ]
+  }
+}
